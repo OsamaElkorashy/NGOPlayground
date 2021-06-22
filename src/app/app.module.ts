59a26c91c4da588992669f1fe4f3b4b4ae3ca697
Osama Elkorashy <https://github.com/OsamaElkorashy>
2021-06-22 20:48:28 +0200

    New NGO Projecct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumberComponent } from './number/number.component';
+import { TextComponent } from './text/text.component';
+import { DateComponent } from './date/date.component';
+import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
+import { FormButtonsComponent } from './form-buttons/form-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberComponent,
+    TextComponent,
+    DateComponent,
+    ConfirmationModalComponent,
+    FormButtonsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-buttons/form-buttons.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-form-buttons',
+  templateUrl: './form-buttons.component.html',
+  styleUrls: ['./form-buttons.component.css']
+})
+export class FormButtonsComponent implements OnInit {
+  @Input() saveText:string;
+  @Input() initialSaveText:string;
+  @Input() cancelText:string;
+  @Input() deleteText:string;
+  @Input() disabledButtons:any;
+
+  @Output() InitialSave = new EventEmitter<null>();
+  @Output() Save = new EventEmitter<null>();
+  @Output() Cancel = new EventEmitter<null>();
+  @Output() Delete = new EventEmitter<null>();
+
+
+
+  constructor() {
+    this.saveText = "ارسال الطلب";
+    this.initialSaveText = "حفظ مبدئي";
+    this.deleteText = "حذف";
+    this.cancelText = "الغاء";
+  }
+
+  ngOnInit(): void {
+  }
+}
